17c1304b3eb5347148b7eb0b9006dbe38919fbc5
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-09-08 23:19:11 +0300

    - перевли логи в отдельную директорию - обработали /start - поставили правильный уровень на логгеры - переписали отстойные сишные строки на модные мультистроки голанга - обработали /close

--- bot.go
+++ bot.go
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	writer, _ = os.OpenFile("log.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	writer, _ = os.OpenFile("log/log.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	log       = logger.Init("Logger", true, true, writer)
 )
 
@@ -140,11 +140,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	b.Handle("/help", func(m *tb.Message) {
-		_, err := b.Send(m.Sender, "хелп в работе")
+	b.Handle("/start", func(m *tb.Message) {
+		_, err := b.Send(m.Sender, `
+Привет!
+Этот бот создан для того чтобы соединять людей у которых есть запросы на работу и людей, готовых выполнять эту работу за деньги
+Команды:
+/add_request - войти в диалог добавления запроса
+/list_requests - показать все открытые и доступные реквесты
+/my_requests - показать все размещенные запросы`)
 
 		if err != nil {
-			log.Fatal(err)
+			log.Error(err)
+		}
+	})
+
+	b.Handle("/help", func(m *tb.Message) {
+		_, err := b.Send(m.Sender, `Этот бот создан для того чтобы соединять людей у которых есть запросы на работу и людей, готовых выполнять эту работу за деньги
+Команды:
+/add_request - войти в диалог добавления запроса
+/list_requests - показать все открытые и доступные реквесты
+/my_requests - показать все размещенные запросы`)
+
+		if err != nil {
+			log.Error(err)
 		}
 	})
 
@@ -153,7 +171,7 @@ func main() {
 			ForceReply: true,
 		})
 		if err != nil {
-			log.Fatal(err)
+			log.Error(err)
 		}
 	})
 
@@ -161,16 +179,21 @@ func main() {
 		reqArr := processDbSelectQuery(p, "select * from requests where state = 'free'")
 		//log.Println("lines retrieved: " + string(len(reqArr)))
 		for _, curReq := range reqArr {
-			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
+			_, err := b.Send(m.Sender, fmt.Sprintf(`
+%s
+id: %d
+Выполнить до: %s
+Цена: %f`, curReq.RequestDescription, curReq.Id, curReq.CompletionDate, curReq.Price))
+
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		}
 
 		if len(reqArr) == 0 {
-			_, err := b.Send(m.Sender, "No free requests currently")
+			_, err := b.Send(m.Sender, "К сожалению, пока что нет свободных запросов. Заходите позже :(")
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		}
 	})
@@ -179,32 +202,44 @@ func main() {
 		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where user_id = %d", m.Sender.ID))
 		//log.Println("lines retrieved: " + string(len(reqArr)))
 		for _, curReq := range reqArr {
-			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
+			_, err := b.Send(m.Sender, fmt.Sprintf(`
+%s
+id: %d
+Выполнить до: %s
+Цена: %f`, curReq.RequestDescription, curReq.Id, curReq.CompletionDate, curReq.Price))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		}
 	})
 
 	b.Handle("/accept", func(m *tb.Message) {
-		requestId, _ := strconv.Atoi(m.Payload)
-		//search the fucking db
-		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where id = %d", requestId))
+		requestId, err := strconv.Atoi(m.Payload)
+
+		if err != nil {
+			log.Error(err)
+			_, err = b.Send(m.Sender, fmt.Sprintf("%s не является числом, а айдишник - только число(", m.Payload))
+			if err != nil {
+				log.Error(err)
+			}
+		}
+
+		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where id = %d and state = ''", requestId))
 		if len(reqArr) == 1 {
-			processDbQuery(p, fmt.Sprintf("update requests set executor_id = %d, executor_name = '%s', state = 'in_work' where id = %d", m.Sender.ID, m.Sender.Username, requestId))
+			processDbQuery(p, fmt.Sprintf("update requests set executor_id = %d, executor_name = '%s' state = 'in_work' where id = %d", m.Sender.ID, m.Sender.Username, requestId))
 			_, err := b.Send(m.Sender, fmt.Sprintf("Реквест %d принят и закреплен за вами!", requestId))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 			_, err = b.Send(&tb.User{ID: int(reqArr[0].UserID)}, fmt.Sprintf("Реквест %d принят пользователем @%s!", requestId, m.Sender.Username))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 
 		} else if len(reqArr) == 0 {
-			_, err := b.Send(m.Sender, fmt.Sprintf("your request is NOT found!"))
+			_, err := b.Send(m.Sender, fmt.Sprintf("Запрос с номером %d не существует", requestId))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		} else {
 			//todo fucking die
@@ -214,10 +249,57 @@ func main() {
 	b.Handle("/accepted_requests", func(m *tb.Message) {
 		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where executor_id = %d", m.Sender.ID))
 		for _, curReq := range reqArr {
-			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
+			_, err := b.Send(m.Sender, fmt.Sprintf(`
+%s
+id: %d
+Выполнить до: %s
+Цена: %f`, curReq.RequestDescription, curReq.Id, curReq.CompletionDate, curReq.Price))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
+		}
+	})
+
+	b.Handle("/close", func(m *tb.Message) {
+		requestId, err := strconv.Atoi(m.Payload)
+
+		if err != nil {
+			log.Error(err)
+			_, err = b.Send(m.Sender, fmt.Sprintf("%s не является числом, а айдишник - только число(", m.Payload))
+			if err != nil {
+				log.Error(err)
+			}
+		}
+
+		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where id = %d and state = 'in_work'", requestId))
+
+		if len(reqArr) == 1 {
+
+			if reqArr[0].UserID != int64(m.Sender.ID) {
+				_, err = b.Send(m.Sender, "Нельзя закрыть не свой реквест")
+				if err != nil {
+					log.Error(err)
+				}
+				return
+			}
+
+			processDbQuery(p, fmt.Sprintf("update requests set state = 'complete' where id = %d", requestId))
+			_, err := b.Send(m.Sender, fmt.Sprintf("Реквест %d закрыт!", requestId))
+			if err != nil {
+				log.Error(err)
+			}
+			_, err = b.Send(&tb.User{ID: int(reqArr[0].ExecutorID.Int64)}, fmt.Sprintf("Реквест %d закрыт!", requestId))
+			if err != nil {
+				log.Error(err)
+			}
+
+		} else if len(reqArr) == 0 {
+			_, err := b.Send(m.Sender, fmt.Sprintf("Запрос с номером %d не существует", requestId))
+			if err != nil {
+				log.Error(err)
+			}
+		} else {
+			//todo fucking die
 		}
 	})
 
@@ -233,7 +315,7 @@ func main() {
 				ForceReply: true,
 			})
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		} else if isReplyTo.Text == "Задайте предположительную дату выполнения реквеста" {
 			log.Info(m.Text)
@@ -242,25 +324,24 @@ func main() {
 				ForceReply: true,
 			})
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		} else if isReplyTo.Text == "Задайте предположительную цену реквеста" {
 			log.Info(m.Text)
-			//todo how to fucking do regexp....
-			//matched, err := regexp.MatchString("^([0-9]+\.?[0-9]*|\.[0-9]+)$", m.Text)
-			//if !matched {
-			//	todo do a loopback
-			//log.Println("DIE!!!!")
-			//}
-			priceVal, _ := strconv.ParseFloat(m.Text, 64)
+			priceVal, err := strconv.ParseFloat(m.Text, 64)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			requests[m.Sender.ID].Price = priceVal
 			req := requests[m.Sender.ID]
 			strRequest := fmt.Sprintf("insert into requests (user_id, user_name, request_desc, price, completion_time) values (%d, '%s', '%s', %f, '%s')", m.Sender.ID, m.Sender.Username, req.RequestDescription, req.Price, req.CompletionDate)
 			id := processDbInsertQuery(p, strRequest)
 			log.Infof("id of latest request is %d", id)
-			_, err := b.Send(m.Sender, fmt.Sprintf("Запрос %d успешно добавлен в базу", id))
+			_, err = b.Send(m.Sender, fmt.Sprintf("Запрос %d успешно добавлен в базу", id))
 			if err != nil {
-				log.Fatal(err)
+				log.Error(err)
 			}
 		}
 	})
