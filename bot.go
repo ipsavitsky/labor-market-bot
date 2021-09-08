a08c24c4248e42dd849988bb473285a30b60a9c3
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-09-08 19:02:53 +0300

    хочу сдохнуть  - переработана обработка ошибок(чтобы в логах была адекватная строка)  - работает вывод добавленного айдишника

--- bot.go
+++ bot.go
@@ -34,71 +34,100 @@ type Request struct {
 
 type requestRepo map[int]*Request
 
-func errCheck(err error) {
+func processDbQuery(p *configparser.ConfigParser, query string) {
+	dbDriver, err := p.Get("DB", "DRIVER")
+
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func processDbQuery(p *configparser.ConfigParser, query string) {
-	dbDriver, err := p.Get("DB", "DRIVER")
-	errCheck(err)
 
 	dbCreds, err := p.Get("DB", "CREDENTIALS")
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	db, err := sqlx.Open(dbDriver, dbCreds)
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	db.MustExec(query)
 }
 
-func processDbSelectQuery(p *configparser.ConfigParser, selectQuery string) []Request {
+func processDbInsertQuery(p *configparser.ConfigParser, query string) int {
 	dbDriver, err := p.Get("DB", "DRIVER")
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	dbCreds, err := p.Get("DB", "CREDENTIALS")
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	db, err := sqlx.Open(dbDriver, dbCreds)
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := db.MustExec(query)
+
+	resInt, err := res.LastInsertId()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(resInt)
+}
+
+func processDbSelectQuery(p *configparser.ConfigParser, selectQuery string) []Request {
+	dbDriver, err := p.Get("DB", "DRIVER")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbCreds, err := p.Get("DB", "CREDENTIALS")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sqlx.Open(dbDriver, dbCreds)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var res []Request
 
 	err = db.Select(&res, selectQuery)
-	errCheck(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	return res
-}
-
-func getLastId(p *configparser.ConfigParser) int {
-	dbDriver, err := p.Get("DB", "DRIVER")
-	errCheck(err)
-
-	dbCreds, err := p.Get("DB", "CREDENTIALS")
-	errCheck(err)
-
-	db, err := sqlx.Open(dbDriver, dbCreds)
-	errCheck(err)
-
-	//todo always returns 0
-	res := struct {
-		id int `db:"last_insert_id()"`
-	}{}
-	err = db.Select(&res, "select last_insert_id()")
-	if err != nil {
-		return 0
-	}
-	return res.id
 }
 
 func main() {
 
 	p, err := configparser.NewConfigParserFromFile("default.cfg")
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	apiToken, err := p.Get("TELEGRAM", "API_TOKEN")
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	requests := make(requestRepo)
 
@@ -106,18 +135,26 @@ func main() {
 		Token:  apiToken,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
-	errCheck(err)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	b.Handle("/help", func(m *tb.Message) {
 		_, err := b.Send(m.Sender, "хелп в работе")
-		errCheck(err)
+
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	b.Handle("/add_request", func(m *tb.Message) {
 		_, err := b.Send(m.Sender, "Введите описание запроса пж", &tb.ReplyMarkup{
 			ForceReply: true,
 		})
-		errCheck(err)
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	b.Handle("/list_requests", func(m *tb.Message) {
@@ -125,12 +162,16 @@ func main() {
 		//log.Println("lines retrieved: " + string(len(reqArr)))
 		for _, curReq := range reqArr {
 			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 
 		if len(reqArr) == 0 {
 			_, err := b.Send(m.Sender, "No free requests currently")
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 
@@ -139,22 +180,32 @@ func main() {
 		//log.Println("lines retrieved: " + string(len(reqArr)))
 		for _, curReq := range reqArr {
 			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 
 	b.Handle("/accept", func(m *tb.Message) {
-		requestId, err := strconv.Atoi(m.Payload)
-		errCheck(err)
+		requestId, _ := strconv.Atoi(m.Payload)
 		//search the fucking db
 		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where id = %d", requestId))
 		if len(reqArr) == 1 {
 			processDbQuery(p, fmt.Sprintf("update requests set executor_id = %d, executor_name = '%s', state = 'in_work' where id = %d", m.Sender.ID, m.Sender.Username, requestId))
-			_, err = b.Send(m.Sender, fmt.Sprintf("Реквест %d принят и закреплен за вами!", requestId))
+			_, err := b.Send(m.Sender, fmt.Sprintf("Реквест %d принят и закреплен за вами!", requestId))
+			if err != nil {
+				log.Fatal(err)
+			}
 			_, err = b.Send(&tb.User{ID: int(reqArr[0].UserID)}, fmt.Sprintf("Реквест %d принят пользователем @%s!", requestId, m.Sender.Username))
+			if err != nil {
+				log.Fatal(err)
+			}
 
 		} else if len(reqArr) == 0 {
-			_, err = b.Send(m.Sender, fmt.Sprintf("your request is NOT found!"))
+			_, err := b.Send(m.Sender, fmt.Sprintf("your request is NOT found!"))
+			if err != nil {
+				log.Fatal(err)
+			}
 		} else {
 			//todo fucking die
 		}
@@ -164,7 +215,9 @@ func main() {
 		reqArr := processDbSelectQuery(p, fmt.Sprintf("select * from requests where executor_id = %d", m.Sender.ID))
 		for _, curReq := range reqArr {
 			_, err := b.Send(m.Sender, fmt.Sprintf("id: %d\nmessage: %s\nprice: %f\nexp_date: %s", curReq.Id, curReq.RequestDescription, curReq.Price, curReq.CompletionDate))
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 
@@ -179,14 +232,18 @@ func main() {
 			_, err := b.Send(m.Sender, "Задайте предположительную дату выполнения реквеста", &tb.ReplyMarkup{
 				ForceReply: true,
 			})
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		} else if isReplyTo.Text == "Задайте предположительную дату выполнения реквеста" {
 			log.Info(m.Text)
 			requests[m.Sender.ID].CompletionDate = m.Text
-			_, err = b.Send(m.Sender, "Задайте предположительную цену реквеста", &tb.ReplyMarkup{
+			_, err := b.Send(m.Sender, "Задайте предположительную цену реквеста", &tb.ReplyMarkup{
 				ForceReply: true,
 			})
-			errCheck(err)
+			if err != nil {
+				log.Fatal(err)
+			}
 		} else if isReplyTo.Text == "Задайте предположительную цену реквеста" {
 			log.Info(m.Text)
 			//todo how to fucking do regexp....
@@ -195,16 +252,16 @@ func main() {
 			//	todo do a loopback
 			//log.Println("DIE!!!!")
 			//}
-			priceVal, err := strconv.ParseFloat(m.Text, 64)
-			errCheck(err)
+			priceVal, _ := strconv.ParseFloat(m.Text, 64)
 			requests[m.Sender.ID].Price = priceVal
 			req := requests[m.Sender.ID]
 			strRequest := fmt.Sprintf("insert into requests (user_id, user_name, request_desc, price, completion_time) values (%d, '%s', '%s', %f, '%s')", m.Sender.ID, m.Sender.Username, req.RequestDescription, req.Price, req.CompletionDate)
-			processDbQuery(p, strRequest)
-			//todo fix this shit
-			id := getLastId(p)
-			_, err = b.Send(m.Sender, fmt.Sprintf("Запрос %d успешно добавлен в базу", id))
-			errCheck(err)
+			id := processDbInsertQuery(p, strRequest)
+			log.Infof("id of latest request is %d", id)
+			_, err := b.Send(m.Sender, fmt.Sprintf("Запрос %d успешно добавлен в базу", id))
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
 
--- database_schema.sql
+++ database_schema.sql
@@ -2,7 +2,7 @@ create database labor_db;
 use labor_db;
 create table requests
 (
-    id               int                                  not null auto_increment,
+    id               int                                  auto_increment primary key,
     user_id          bigint                               not null,
     executor_id      bigint                                        default null,
     user_name        varchar(255)                         not null,
@@ -11,6 +11,5 @@ create table requests
     state            enum ('free', 'in_work', 'complete') not null default 'free',
     price            decimal(7, 2)                        not null,
     creation_time    datetime                             not null default current_timestamp,
-    completion_time  text                                 not null,
-    primary key (id)
+    completion_time  text                                 not null
 )
